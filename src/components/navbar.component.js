050ca69ca9bd6ec5c899a5460fbf4908797f1730
Rahul <rrr392@nyu.edu>
2020-06-25 00:50:47 -0400

    cba

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -5,11 +5,13 @@ function Navbar(props) {
   return (
     <nav>
       {/* link instead of anchor tag from react-router. 'to' param shows where it links to.*/}
-      <Link to="/" className="display-4">{props.appName}</Link>
+      {/* className="display-4" */}
+      <Link to="/">{props.appName}</Link>
       <div className="navbar-collapse">
         <ul className='navbar-nav mr-auto'>
           {/* <li className='navbar-item display-5 btn-link'><Link to='/'>{props.username}</Link></li> */}
-          <li className='navbar-item display-5 btn-link'
+          {/* display-5  */}
+          <li className='navbar-item btn-link'
             onClick={props.signout}>
             <Link to='/create' onClick={props.signoutToggleFunc}>{props.signedOutFlag ? null : 'Sign Out'}
             </Link>
