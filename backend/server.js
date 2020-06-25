2c486cf3cc23516aaf27d6e36968caf18e73bb71
Rahul <rrr392@nyu.edu>
2020-06-25 01:29:23 -0400

    cba

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, '../build')))
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build'))
+    res.sendFile(path.join(__dirname, '../build/index.html'))
 })
 const port = process.env.PORT || 5000;
 
--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -5,13 +5,11 @@ function Navbar(props) {
   return (
     <nav>
       {/* link instead of anchor tag from react-router. 'to' param shows where it links to.*/}
-      {/* className="display-4" */}
-      <Link to="/">{props.appName}</Link>
+      <Link to="/" className="display-4">{props.appName}</Link>
       <div className="navbar-collapse">
         <ul className='navbar-nav mr-auto'>
           {/* <li className='navbar-item display-5 btn-link'><Link to='/'>{props.username}</Link></li> */}
-          {/* display-5  */}
-          <li className='navbar-item btn-link'
+          <li className='navbar-item display-5 btn-link'
             onClick={props.signout}>
             <Link to='/create' onClick={props.signoutToggleFunc}>{props.signedOutFlag ? null : 'Sign Out'}
             </Link>
