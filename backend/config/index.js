3380dcbd05f73b804d346ecd67b2e4dccaccbbcc
Rahul Ramachandran <rahul.rcd96@gmail.com>
2020-06-29 20:04:32 -0400

    Delete index.js

--- backend/config/index.js
+++
@@ -1,7 +0,0 @@
-const dbuser = 'rahul';
-const dbpassword = '7sHN5ib5RVQ0YKWn';
-
-const MONGODB_URI = `mongodb+srv://${dbuser}:${dbpassword}@cluster0-844ck.gcp.mongodb.net/test?retryWrites=true&w=majority`;
-
-module.exports = MONGODB_URI;
-
